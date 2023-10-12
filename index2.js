1a2c8d5701ce5d2ce10387ec00202a40a7c9a706
aalskdjfhg123 <jiangryan8@gmail.com>
2023-10-12 13:15:53 -0400

    Update index2.js

--- index2.js
+++ index2.js
@@ -1,5 +1,28 @@
-const Domselectors = {
+/* const Domselectors = {
     Text: document.getElementById("text"),
     text2: document.getElementById("text2")
 };
-console.log(Domselectors.Text)
+console.log(Domselectors.Text) */
+const Domselectors = {
+  form: document.getElementById("form"),
+  entered: document.querySelector("#enterText"),
+  h2: document.querySelectorAll("h2"),
+  button: document.getElementById("button")
+}
+
+/* console.log(Domselectors.h2);
+Domselectors.form.addEventListener("submit", function(event){
+    event.preventDefault();
+
+    Domselectors.h2.forEach((text) => text.textContent = Domselectors.entered.value
+        
+    );
+}) */
+
+function textChange(text){
+   text.textContent = "uheuhufhufhufehfuehuefhuhuhuhuhuhuh"
+}
+
+Domselectors.button.addEventListener("click", function(event){
+textChange(Domselectors.h2);
+})
