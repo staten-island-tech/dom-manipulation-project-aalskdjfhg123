c4015e22055f0abf9e08f374888113bbd9085bde
aalskdjfhg123 <jiangryan8@gmail.com>
2023-10-19 18:28:12 -0400

    Update index.js

--- index.js
+++ index.js
@@ -3,35 +3,50 @@ const Domselectors = {
     container: document.querySelector(".container"),
     form: document.getElementById("form"),
     entered: document.getElementById("entered")
-}
+}//dom
+
+Domselectors.entered.value = '#';
+//makes the input box start with the #
 
 let boxCount = 0
 let doCount = 0
+/* to count how many boxes there are and how many times the user has
+clicked the button */
+
+
 const hexTest = /^#([0-9A-Fa-f]{3}){1,2}$/;
-Domselectors.entered.value = '#';
+//checks to see if inputed hex code is valid
 
 Domselectors.button.addEventListener("click", function () {
     
-    if (boxCount <= 7 && hexTest.test(Domselectors.entered.value)) {
+    if (boxCount <= 7 && hexTest.test(Domselectors.entered.value))//checks if there are too many boxes or if there is an invalid hex code 
+    {
 
         const newBox = document.createElement("div");
         newBox.style.backgroundColor = Domselectors.entered.value
         newBox.classList.add("box");
-        Domselectors.entered.value = '#';
+        //creates a new box, sets the color of the box, then adds it to the "box" class so the css applies
+
+
+        Domselectors.entered.value = '#';//resets the input field
 
 
         newBox.addEventListener("click", function () {
 
             Domselectors.container.removeChild(newBox);
             boxCount--;
+            //removes box 
         });
 
         Domselectors.container.appendChild(newBox);
         boxCount++
+        //adds box to the page
+
     } else if (boxCount >= 8) {
-        alert("chill out bro too many boxes");
+        alert("chill out bro too many boxes");//checks if too many boxes
     }
-    else {
+    else //checks if invalid hex
+    {
         const badBox = document.createElement("div");
         badBox.classList.add("angryBox");
 
@@ -52,6 +67,7 @@ Domselectors.button.addEventListener("click", function(){
     if (doCount < 1){
         alert("click on boxes you dont like")
         doCount++
+        //tells user how to delete boxes
     }
     
 })
