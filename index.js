74ef1add1fe3196e260a1518e7ce22c436a8dff5
aalskdjfhg123 <jiangryan8@gmail.com>
2023-10-24 13:16:35 -0400

    updte

--- index.js
+++ index.js
@@ -4,7 +4,10 @@ const Domselectors = {
     form: document.getElementById("form"),
     entered: document.getElementById("entered"),
     deleteButton: document.getElementById("delete"),
-    sampleButton: document.getElementById("sample")
+    sampleButton: document.getElementById("sample"),
+    /*  redIn: document.getElementById("red"),
+     blueIn: document.getElementById("blue"),
+     greenIn: document.getElementById("green"), */
 }//dom
 
 Domselectors.entered.value = '#';
@@ -16,24 +19,22 @@ let doCount = 0
 clicked the button */
 
 
-const hexTest = /^#([0-9A-Fa-f]{3}){1,2}$/;
+const hexTest = /^#([0-9A-Fa-f]{3}){1,2}$/;  //as a side effect it also takes numbers as input
 //checks to see if inputed hex code is valid
 
 
-
 Domselectors.button.addEventListener("click", function () {
-
+    /*   const red = Domselectors.redIn.value
+      const blue = Domselectors.blueIn.value
+      const green = Domselectors.greenIn.value */
     if (boxCount <= 7 && hexTest.test(Domselectors.entered.value))//checks if there are too many boxes or if there is an invalid hex code 
     {
-
         const newBox = document.createElement("div");
         newBox.style.backgroundColor = Domselectors.entered.value
         newBox.classList.add("box");
         //creates a new box, sets the color of the box, then adds it to the "box" class so the css applies
 
-
         Domselectors.entered.value = '#';//resets the input field
-
 
         newBox.addEventListener("click", function () {
 
@@ -46,7 +47,25 @@ Domselectors.button.addEventListener("click", function () {
         boxCount++
         //adds box to the page
 
-    } else if (boxCount >= 8) {
+    } /* else if (boxCount <= 7 && (red >= 0 && red <= 255 && green >= 0 && green <= 255 && blue >= 0 && blue <= 255)) {
+
+        const newBox = document.createElement("div");
+        newBox.style.backgroundColor = 'rgb(' + red + ',' + green + ',' + blue + ')';
+        newBox.classList.add("box");
+
+
+        Domselectors.entered.value = '#';
+
+
+        newBox.addEventListener("click", function () {
+
+            Domselectors.container.removeChild(newBox);
+            boxCount--;
+        });
+
+        Domselectors.container.appendChild(newBox);
+        boxCount++
+    }  */ else if (boxCount >= 8) {
         alert("chill out buddy too many boxes");//checks if too many boxes
     }
     else //checks if invalid hex
@@ -80,6 +99,7 @@ Domselectors.deleteButton.addEventListener("click", function () {
     if (boxCount > 0) {
         while (Domselectors.container.firstChild) {
             Domselectors.container.removeChild(Domselectors.container.firstChild, boxCount = 0);
+            Domselectors.entered.value = '#';
         }//deletes all boxes
     }
     else {
@@ -87,11 +107,9 @@ Domselectors.deleteButton.addEventListener("click", function () {
     }//checks if there are any boxes to delete
 })
 
-
 Domselectors.sampleButton.addEventListener("click", function () {
 
     let number = Math.floor(Math.random() * (5) + 1)
-    console.log(number)
     if (number == 1) {
         Domselectors.entered.value = "#D65858"
     } else if (number == 2) {
